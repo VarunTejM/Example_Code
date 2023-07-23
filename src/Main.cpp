07b95adc0adfd8a98c526a34102ebff6cfe391db
Sakal 010 User <sklusr010@Sakal.local>
2023-07-23 23:38:55 +0200

    my first source file

---
+++ src/Main.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+int main()
+{
+	int x;
+	std::cout << "enter value \n";
+	std::cin >> x;
+
+	if (x == 1)
+	{
+		std::cout << "Hello World";
+	}
+	else if(x==2)
+	{
+		std::cout << "VARUN";
+	}
+
+		
+	return 0;
+}
